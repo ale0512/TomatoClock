cf4faf04fc7b297c06bef3a5c081e7d4ca5d5417
ale0512 <ale0512@126.com>
2011-11-08 18:35:06 +0800

    增加自定义图片按钮

--- gen/org/ale/tomato/R.java
+++ gen/org/ale/tomato/R.java
@@ -10,29 +10,39 @@ package org.ale.tomato;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f050001;
+        public static final int grey=0x7f050002;
+        public static final int red=0x7f050000;
+        public static final int white=0x7f050003;
+    }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int button_focused=0x7f020000;
+        public static final int button_normal=0x7f020001;
+        public static final int button_pressed=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int submit=0x7f020004;
     }
     public static final class id {
-        public static final int bt_start=0x7f060001;
-        public static final int bt_stop=0x7f060002;
-        public static final int individual_time_remaining=0x7f060000;
+        public static final int bt_start=0x7f070001;
+        public static final int bt_stop=0x7f070002;
+        public static final int individual_time_remaining=0x7f070000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int bg_color=0x7f050001;
-        public static final int bt_start=0x7f050002;
-        public static final int bt_stop=0x7f050003;
-        public static final int dialog_msg=0x7f050005;
-        public static final int dialog_no=0x7f050007;
-        public static final int dialog_title=0x7f050004;
-        public static final int dialog_yes=0x7f050006;
-        public static final int menu_about=0x7f05000a;
-        public static final int menu_setting=0x7f050009;
-        public static final int menu_task=0x7f050008;
+        public static final int app_name=0x7f060000;
+        public static final int bg_color=0x7f060001;
+        public static final int bt_start=0x7f060002;
+        public static final int bt_stop=0x7f060003;
+        public static final int dialog_msg=0x7f060005;
+        public static final int dialog_no=0x7f060007;
+        public static final int dialog_title=0x7f060004;
+        public static final int dialog_yes=0x7f060006;
+        public static final int menu_about=0x7f06000a;
+        public static final int menu_setting=0x7f060009;
+        public static final int menu_task=0x7f060008;
     }
     public static final class xml {
         public static final int settings=0x7f040000;
